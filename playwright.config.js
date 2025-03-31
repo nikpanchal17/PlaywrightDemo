7facc49c22edb5d23c78ffe0089e67f485c47ab9
nikpanchal17 <nik20011711@gmail.com>
2025-03-31 14:08:18 +0530

    Allure Report

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -39,9 +39,9 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] ,
-            screenshot:"on",
-            video:"on",
-            trace:"on"
+            // screenshot:"on",
+            // video:"on",
+            // trace:"on"
       },
 
     },
